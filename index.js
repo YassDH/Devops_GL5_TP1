80451c1e407b980019a8d2ce91b780043b7f09fd
Dhaouadi Yassine <yassine.dhaouadi@insat.u-carthage.tn>
2024-10-18 16:03:18 +0100

    Added new endpoint

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-}
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port} !`);
+});
 
 module.exports = app;
